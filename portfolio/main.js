1f6e060bfe1b5f858085b4a3b994de8ec9b61540
michaeladamstrickland <strickland1514@gmail.com>
2019-07-17 20:15:45 -0600

    new portfolio

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,37 +1,10 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-        var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos) {
-                document.getElementById("navbar").style.top = "0";
-        } else {
-                document.getElementById("navbar").style.top = "-50px";
-        }
-        prevScrollpos = currentScrollPos;
-}
-
-
-
-
-$(document).ready(function () {
-        $('body').css('background', $("#one").attr("data-color"));
-        $('.btn1').click(function () {
-                $('header').hide(300);
-        })
-        $('#content_body').hide();
-        $('.btn1').click(function () {
-                $('#content_body').show(300);
-        });
-
-
-});
-
 var one = "#10e88a",
         two = "#7f3995",
         three = "#0d78e7",
         four = "#f88f18",
         five = "#bb6774";
 
-$(window).on("scroll touchmove", function () {
+$(window).on("scroll touchmove", function() {
         if ($(document).scrollTop() >= $("#one").position().top) {
                 $('body').css('background', $("#one").attr("data-color"));
 
